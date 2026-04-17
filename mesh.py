5137b259b9d72f6f309e62952f9cbf4887e9f2f1
Lucas Moidel <lucas122133@gmail.com>
2026-04-17 00:50:28 -0700

    improve error handling

--- mesh.py
+++ mesh.py
@@ -9,7 +9,6 @@ letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
 
 
 status = ""
-lock = threading.Lock()
 target = ""
 interface = 0
 
@@ -24,6 +23,10 @@ turn = False
 
 winner = 0
 
+gotResponseAttack = False
+gotResponseMissHit = False
+gotResponseWin = False
+
 enemy = [[]]
 player = []
 guesses = [[-1, -1]]
@@ -37,28 +40,50 @@ def attack(x, y):
     sendPacket(0, x, y)
     
 
+def repeat(i, x=0, y=0):
+    if i == 0 and not gotResponseAttack:
+        print("try " + str(i))
+        sendPacket(i, x, y)
+    elif (i == 1 or i == 2) and not gotResponseMissHit:
+        print("try " + str(i))
+        sendPacket(i, x, y)
+    elif i == -3 and not gotResponseWin:
+        print("try " + str(i))
+        sendPacket(i, x, y)
 
+        
+        
 
 def sendPacket(i, x=0, y=0):
     global status
     global gameStarted
     global turn
     global winner
+    global gotResponseAttack
+    global gotResponseMissHit
+    global gotResponseWin
     if i == 0:
         interface.sendText(("attack " + str(x) + " " + str(y)), channelIndex=1, wantAck=True)
         status = "attacking " + letters[y] + str(x+1)
+        threading.Timer(10, repeat, [i, x, y]).start()
     elif i == 1:
         interface.sendText(("hit " + str(x) + " " + str(y)), channelIndex=1, wantAck=True)
         status = "the enemy has hit your boat at " + letters[y] + str(x+1)
+        threading.Timer(10, repeat, [i, x, y]).start()
     elif i == 2:
         interface.sendText(("miss " + str(x) + " " + str(y)), channelIndex=1, wantAck=True)
         status = "the enemy has missed at " + letters[y] + str(x+1)
+        threading.Timer(10, repeat, [i, x, y]).start()
                 
 
     elif i == 3:
         interface.sendText(("ok hit " + str(x) + " " + str(y)), channelIndex=1, wantAck=True)
+        
+        
     elif i == 4:
         interface.sendText(("ok miss " + str(x) + " " + str(y)), channelIndex=1, wantAck=True)
+
+        
     elif i == -1:
         interface.sendText("start game", channelIndex=1, wantAck=True)
         status = "init game"
@@ -68,10 +93,14 @@ def sendPacket(i, x=0, y=0):
         gameStarted = True
     elif i == -3:
         time.sleep(2)
-        print("aaaaaaaaaaaaaaaaaaaaaaa")
         interface.sendText("you win", channelIndex=1, wantAck=True)
         status = "you lose. All your ships have been destroyed."
         winner = 2
+        threading.Timer(10, repeat, [i, x, y]).start()
+        
+    elif i == -4:
+        interface.sendText("ok you win", channelIndex=1, wantAck=True)
+        turn = False
         
         
 
@@ -81,6 +110,9 @@ def onReceive(packet, interface, screen):
     global status
     global gameStarted
     global winner
+    global gotResponseAttack
+    global gotResponseMissHit
+    global gotResponseWin
     if len(packet.get('decoded' , "")):
             if len(packet['decoded'].get('payload' , "")):
                 #print(packet['decoded']['payload'])
@@ -92,32 +124,41 @@ def onReceive(packet, interface, screen):
                     status = "starting game"
                     gameStarted = True
                     turn = True
-                print(packet['decoded']['payload'])
                 if packet['decoded']['payload'][:7] == b'you win':
-                    print("aaaaaaaaaaaaaaaaaaaaaaa")
                     status = "you win. All the enemy's ships have been destroyed."
                     winner = 1
+                    sendPacket(-4)
+                if packet['decoded']['payload'][:10] == b'ok you win':
+                    gotResponseWin = True
                 if packet['from'] == target and packet['decoded']['payload'][:3] == b'hit':
                     x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[1])
                     y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
                     enemy[0].append([x, y, False])
                     print(enemy)
                     status = "hit " + letters[y] + str(x+1)
+                    gotResponseAttack = True
+                    print(gotResponseAttack)
                     sendPacket(3, x, y)
                 if packet['from'] == target and packet['decoded']['payload'][:4] == b'miss':
                     x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[1])
                     y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
                     status = "miss " + letters[y] + str(x+1)
+                    gotResponseAttack = True
+                    print(gotResponseAttack)
                     sendPacket(4, x, y)
                 if packet['from'] == target and packet['decoded']['payload'][:6] == b'ok hit':
-                    x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[1])
-                    y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
-                    
+                    x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
+                    y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[3])
+                    gotResponseMissHit = True
                     turn = True
+                    gotResponseAttack = False
                 if packet['from'] == target and packet['decoded']['payload'][:7] == b'ok miss':
-                    x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[1])
-                    y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
+                    x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
+                    y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[3])
+                    gotResponseMissHit = True
                     turn = True
+                    gotResponseAttack = False
+                    print(gotResponseAttack)
                 if packet['from'] == target and packet['decoded']['payload'][:6] == b'attack':
                     x = int(packet['decoded']['payload'].decode('utf-8').split(" ")[1])
                     y = int(packet['decoded']['payload'].decode('utf-8').split(" ")[2])
